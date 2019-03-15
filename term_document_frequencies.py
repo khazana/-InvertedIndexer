ef098ad0283a622bc9a9d6a2d5611580278f1962
khazana <haiyum95@gmail.com>
2019-03-15 15:55:10 -0400

    Update term_document_frequencies.py

--- term_document_frequencies.py
+++ term_document_frequencies.py
@@ -38,11 +38,30 @@ def doc_frequency(term_dict):
     doc_dict = dict(sorted(document_frequency.items(), key=lambda kv: kv[0]))
     return doc_dict
 
-
 def write_dict_to_file(dictionary, dictionary_name):
      with open(dictionary_name + '.txt', "a") as f:
          f.writelines('{}:{} \n'.format(k,v) for k, v in dictionary.items())
- 
+
+#generates stoplist from dictionaries stored in file 
+def generate_stop_list(file):
+    stoplist = []
+    b = {}
+    f = open(file, 'r')
+    term_list = f.readlines()
+    term_list = [t.strip() for t in term_list]
+    f.close()
+    for i in term_list:
+        k,v = i.split(':')
+        v = v.split(',')
+        for value in v:
+          b.setdefault(k,[]).append(value)
+    for k,v in b.items():
+        count = v[len(v) - 1]
+        count = int(count.replace(']',''))
+        if count/955 > 0.5:
+            stoplist.append(k)
+    
+    
 #Get inverted lists for each unigrams, bigrams and trigrams
 unigram_dict = get_dict('InvertedLists/unigram_indexer.txt')
 bigram_dict = get_dict('InvertedLists/bigram_indexer.txt')
@@ -50,21 +69,19 @@ trigram_dict = get_dict('InvertedLists/trigram_indexer.txt')
 
 #term frequency
 unigram_term_frequency = terms_frequency(unigram_dict)
-bigram_term_frequency = terms_frequency(bigram_dict)
-trigram_term_frequency = terms_frequency(trigram_dict )
-
+bigram_term_frequency1 = terms_frequency(bigram_dict)
+trigram_term_frequency = terms_frequency(trigram_dict)
 
 write_dict_to_file(unigram_term_frequency, get_var_name(unigram_term_frequency)[0])
-write_dict_to_file(bigram_term_frequency, get_var_name(bigram_term_frequency)[0])
+write_dict_to_file(bigram_term_frequency1, get_var_name(bigram_term_frequency1)[0])
 write_dict_to_file(trigram_term_frequency,get_var_name(trigram_term_frequency)[0])
-
-
 
 #document frequency
 unigram_doc_frequency = doc_frequency(unigram_dict)
-bigram_doc_frequency = doc_frequency(bigram_dict)
+bigram_doc_frequency1 = doc_frequency(bigram_dict)
 trigram_doc_frequency = doc_frequency(trigram_dict)
 
 write_dict_to_file(unigram_doc_frequency, get_var_name(unigram_doc_frequency)[0])
-write_dict_to_file(bigram_doc_frequency, get_var_name(bigram_doc_frequency)[0])
+write_dict_to_file(bigram_doc_frequency1, get_var_name(bigram_doc_frequency1)[0])
 write_dict_to_file(trigram_doc_frequency,get_var_name(trigram_doc_frequency)[0])
+
